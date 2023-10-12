d88a4f8b40efc34ec1b282daca48b31b7773bdfc
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2023-10-12 15:28:21 -0500

    Create data5.sh

---
+++ data5.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd /
+tar -zxvf ccc
+mv colon_chameleon colon
+cd colon
+find -name '._*' -delete
+cd test
+cd colon_aca
+cd test_aca5
+mv *jpeg /colon/test/colon_aca
+cd ..
+rm -r test*
+cd ..
+cd colon_n
+cd test_n_5
+mv *jpeg /colon/test/colon_n
+cd ..
+rm -r test*
+cd ../..
+cd train
+cd colon_aca
+cd train_aca_5
+mv *jpeg /colon/train/colon_aca
+cd ..
+rm -r train*
+cd ..
+cd colon_n
+cd train_n_5
+mv *jpeg /colon/train/colon_n
+cd ..
+rm -r train*
+cd ../../..
