1375cf089edc1556b6c1cae64d610a699dc93593
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2023-10-31 18:46:36 -0500

    Update data66.sh

--- data66.sh
+++ data66.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /
-curl -L -o fff https://www.dropbox.com/scl/fi/7siu38wlhtvnc33dxkbsz/ct_80_6_node.tar.gz?rlkey=4ffjm93wiedr8bktyr6qkbpcs&dl=0
-tar -zxvf fff
+#cd /
+#curl -L -o fff https://www.dropbox.com/scl/fi/7siu38wlhtvnc33dxkbsz/ct_80_6_node.tar.gz?rlkey=4ffjm93wiedr8bktyr6qkbpcs&dl=0
+#tar -zxvf fff
 #mv generated_colon_chameleon colon_gen
 cd ct_80
 find -name '._*' -delete
