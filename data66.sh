ee1c7d8fed51ef4718cdb60e8b521e78e8ba3896
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2023-10-31 20:44:52 -0500

    Update data66.sh

--- data66.sh
+++ data66.sh
@@ -8,38 +8,38 @@ rm sp*
 cd train
 rm sp*
 cd NiCT
-cd train_nict_2
+cd train_nict_1
 mv *jpg /ct_80/train/NiCT
 cd ..
 rm -r train*
 cd ..
 cd nCT
-cd train_nct2
+cd train_nct1
 mv *jpg /ct_80/train/nCT
 cd ..
 rm -r train*
 cd ..
 cd pCT
-cd train_pct2
+cd train_pct1
 mv *jpg /ct_80/train/pCT
 cd ..
 rm -r train*
 cd ../..
 cd test
 cd NiCT
-cd test_nict2
+cd test_nict1
 mv *jpg /ct_80/test/NiCT
 cd ..
 rm -r test*
 cd ..
 cd nCT
-cd test_nct2
+cd test_nct1
 mv *jpg /ct_80/test/nCT
 cd ..
 rm -r test*
 cd ..
 cd pCT
-cd test_pct2
+cd test_pct1
 mv *jpg /ct_80/test/pCT
 cd ..
 rm -r test*
