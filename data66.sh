b1c27c795202ca21b97868d1785661ed456fc0d0
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2023-10-31 21:39:00 -0500

    Update data66.sh

--- data66.sh
+++ data66.sh
@@ -6,38 +6,38 @@ cd ct_80
 find -name '._*' -delete
 cd train
 cd NiCT
-cd train_nict_1
+cd train_nict_5
 mv *jpg /ct_80/train/NiCT
 cd ..
 rm -r train*
 cd ..
 cd nCT
-cd train_nct1
+cd train_nct5
 mv *jpg /ct_80/train/nCT
 cd ..
 rm -r train*
 cd ..
 cd pCT
-cd train_pct1
+cd train_pct5
 mv *jpg /ct_80/train/pCT
 cd ..
 rm -r train*
 cd ../..
 cd test
 cd NiCT
-cd test_nict1
+cd test_nict5
 mv *jpg /ct_80/test/NiCT
 cd ..
 rm -r test*
 cd ..
 cd nCT
-cd test_nct1
+cd test_nct5
 mv *jpg /ct_80/test/nCT
 cd ..
 rm -r test*
 cd ..
 cd pCT
-cd test_pct1
+cd test_pct5
 mv *jpg /ct_80/test/pCT
 cd ..
 rm -r test*
