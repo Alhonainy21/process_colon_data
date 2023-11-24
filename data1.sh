143b491dfc09100decb6964f759d0dd13b86007c
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2023-11-24 14:32:59 -0600

    Update data1.sh

--- data1.sh
+++ data1.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 cd /
-curl -L -o sss https://www.dropbox.com/scl/fi/ysusxqjrg0x5p0p92jcbc/6_nodes.tar.gz?rlkey=my8dv4j965vpq4z96o1z0axk9&dl=0
-tar -zxvf sss
-mv 6_nodes data
-cd data
+curl -L -o lll https://www.dropbox.com/scl/fi/hn2d0j12779u9aleoo11x/data48.tar.gz?rlkey=35l8gq0dsq2goyhn6k87qujx0&dl=0
+tar -zxvf lll
+mv data48 data9
+cd data9
 find -name '._*' -delete
 cd test
 cd class_0
 cd test_01
-mv *jpeg /data/test/class_0
+mv *jpeg /data9/test/class_0
 cd ..
 rm -r test*
 cd ..
 cd class_1
 cd test_11
-mv *jpeg /data/test/class_1
+mv *jpeg /data9/test/class_1
 cd ..
 rm -r test*
 cd ../..
@@ -22,13 +22,13 @@ cd train
 cd 
 cd class_0
 cd train_01
-mv *jpeg /data/train/class_0
+mv *jpeg /data9/train/class_0
 cd ..
 rm -r train*
 cd ..
 cd class_1
 cd train_11
-mv *jpeg /data/train/class_1
+mv *jpeg /data9/train/class_1
 cd ..
 rm -r train*
 cd ../../..
